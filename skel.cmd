cd054332e4855b09139ddadfcb69ddc75f2c6e98
Charles-Antoine Degennes <cadegenn@univ-lr.fr>
2018-09-11 09:28:56 +0200

    update header

--- skel.cmd
+++ skel.cmd
@@ -4,31 +4,31 @@ setlocal enableExtensions enableDelayedExpansion
 rem
 rem @file skel.cmd
 rem @brief skeleton of script to use with efunctions.cmd
-rem @author Charles-Antoine Degennes <cadegenn]gmail.com>
-rem @copyright  Copyright (C) 2015-2016  Charles-Antoine Degennes <cadegenn@gmail.com>
+rem @project cmd_fw
+rem @author Charles-Antoine Degennes (cadegenn@gmail.com)
+rem @date Monday September 10th 2018
+rem @copyright (c) 2018 Charles-Antoine Degennes
+rem 
+rem @modified 
+rem @modifiedby 
+rem 
+rem This file is part of Tiny %COMSPEC% Framework
+rem 
+rem        Tiny %COMSPEC% Framework is free software: you can redistribute it and/or modify
+rem        it under the terms of the GNU General Public License as published by
+rem        the Free Software Foundation, either version 3 of the License, or
+rem        (at your option) any later version.
+rem 
+rem        Tiny %COMSPEC% Framework is distributed in the hope that it will be useful,
+rem        but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem        GNU General Public License for more details.
+rem 
+rem        You should have received a copy of the GNU General Public License
+rem        along with Tiny %COMSPEC% Framework.  If not, see <http://www.gnu.org/licenses/>.
 rem
-rem This file is part of TinyCmdFramework
-rem 
-rem     TinyCmdFramework is free software: you can redistribute it and/or modify
-rem     it under the terms of the GNU General Public License as published by
-rem     the Free Software Foundation, either version 3 of the License, or
-rem     (at your option) any later version.
-rem 
-rem     TinyCmdFramework is distributed in the hope that it will be useful,
-rem     but WITHOUT ANY WARRANTY; without even the implied warranty of
-rem     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-rem     GNU General Public License for more details.
-rem 
-rem     You should have received a copy of the GNU General Public License
-rem     along with TinyCmdFramework.  If not, see <http://www.gnu.org/licenses/>.
-rem 
-rem
-rem Changelog
-rem =========
-rem 2016.03.14, DC1 -   add optional parameter -q to silence everything
-rem 2015.03.23, DCA - 	initial version of script
 
-rem initialise window title
+rem initialize window title
 title %0
 
 rem compute DIRNAME
