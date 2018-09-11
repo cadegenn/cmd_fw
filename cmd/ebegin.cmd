fb562218f09e74fe15040b3332ff3d36d3a0001e
Charles-Antoine Degennes <cadegenn@univ-lr.fr>
2018-09-11 12:01:12 +0200

    move e*() functions to cmd/

---
+++ cmd/ebegin.cmd
@@ -0,0 +1,30 @@
+rem
+rem @file ebegin.cmd
+rem @project cmd_fw
+rem @author Charles-Antoine Degennes (cadegenn@gmail.com)
+rem @date 2018.09.10
+rem @copyright (c) 2018 Charles-Antoine Degennes
+rem 
+rem @modified 
+rem @modifiedby 
+rem 
+rem This file is part of Tiny %COMSPEC% Framework
+rem 
+rem        Tiny %COMSPEC% Framework is free software: you can redistribute it and/or modify
+rem        it under the terms of the GNU General Public License as published by
+rem        the Free Software Foundation, either version 3 of the License, or
+rem        (at your option) any later version.
+rem 
+rem        Tiny %COMSPEC% Framework is distributed in the hope that it will be useful,
+rem        but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem        GNU General Public License for more details.
+rem 
+rem        You should have received a copy of the GNU General Public License
+rem        along with Tiny %COMSPEC% Framework.  If not, see <http://www.gnu.org/licenses/>.
+rem 
+rem
+
+rem @brief  print a message on the console
+rem @param	(string)	message without quotes
+if NOT DEFINED QUIET echo %PREPEND% %INDENT:_= %%*
---
+++ cmd/eindent.cmd
@@ -0,0 +1,29 @@
+rem
+rem @file eindent.cmd
+rem @project cmd_fw
+rem @author Charles-Antoine Degennes (cadegenn@gmail.com)
+rem @date 2018.09.10
+rem @copyright (c) 2018 Charles-Antoine Degennes
+rem 
+rem @modified 
+rem @modifiedby 
+rem 
+rem This file is part of Tiny %COMSPEC% Framework
+rem 
+rem        Tiny %COMSPEC% Framework is free software: you can redistribute it and/or modify
+rem        it under the terms of the GNU General Public License as published by
+rem        the Free Software Foundation, either version 3 of the License, or
+rem        (at your option) any later version.
+rem 
+rem        Tiny %COMSPEC% Framework is distributed in the hope that it will be useful,
+rem        but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem        GNU General Public License for more details.
+rem 
+rem        You should have received a copy of the GNU General Public License
+rem        along with Tiny %COMSPEC% Framework.  If not, see <http://www.gnu.org/licenses/>.
+rem 
+rem
+
+rem @brief  indent further text by 2 spaces
+set INDENT=%INDENT%__
